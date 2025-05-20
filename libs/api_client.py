b0ea4b576b1638e4bd6b642c88673be113eb4d2e
VASILIYKAS <yoyoynv@gmail.com>
2025-05-20 12:07:46 +0500

    The code has been moved inside the if __name__ == '__main__': block.

--- libs/api_client.py
+++ libs/api_client.py
@@ -3,13 +3,11 @@ import time
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
-DVMN_API_KEY = os.getenv('DVMN_API_KEY')
-LONG_POLLING_URL = 'https://dvmn.org/api/long_polling/'
-
 
 def check_reviews(last_timestamp=None):
+    DVMN_API_KEY = os.getenv('DVMN_API_KEY')
+    LONG_POLLING_URL = 'https://dvmn.org/api/long_polling/'
+
     params = {'timestamp': last_timestamp} if last_timestamp else {}
     headers = {'Authorization': f'Token {DVMN_API_KEY}'}
 
@@ -39,11 +37,30 @@ def check_reviews(last_timestamp=None):
         }
 
     except requests.exceptions.ReadTimeout:
-        print('Нет ответа от сервера, ожидаем...')
         return {'status': 'timeout'}
     except requests.exceptions.ConnectionError:
-        print('Пропало интернет соединение. Повторное соединение через 5 сек.')
         time.sleep(5)
         return {'status': 'connection_error'}
     except Exception as e:
-        return print(f'Неизвестная ошибка: {str(e)}')
+        return {'status': 'error', 'message': str(e)}
+
+
+def main():
+    load_dotenv()
+    last_timestamp = None
+
+    while True:
+        work_status = check_reviews(last_timestamp)
+
+        if work_status['status'] == 'found':
+            last_timestamp = work_status['timestamp']
+        elif work_status['status'] == 'timeout':
+            print('Нет ответа от сервера, ожидаем...')
+        elif work_status['status'] == 'connection_error':
+            print('Пропало интернет соединение. Повторное соединение через 5 сек.')
+        elif work_status['status'] == 'error':
+            print(f'Неизвестная ошибка: {work_status["message"]}')
+
+
+if __name__ == '__main__':
+    main()
